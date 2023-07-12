9a731a6cf7dac7d4b7490e785faea66effd5055b
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-07-12 11:56:41 -0400

    How a GET request would normally be handled with express

---
+++ app/hello/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/users', (req, res) => {
+    //Handle GET request for /api/users
+    const users = [
+        {id: 1 , name: "Matt"},
+        {id: 2 , name: "Michele"},
+        {id: 3 , name: "Glenn"},
+    ];
+
+//Send the users as a response
+res.json(users);
+})
+
+app.listen(3000, () => {
+    console.log("App is listening to you, even in your sleep...");
+})
