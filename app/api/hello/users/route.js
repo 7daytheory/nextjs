efa59ed84308c91ebacd711d286e14b29a3408de
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-07-12 12:13:52 -0400

    Create a serverless request with Next js

--- app/api/hello/users/route.js
+++ app/api/hello/users/route.js
@@ -1,1 +1,41 @@
+// http://localhost:300/api/users
 
+export async function GET(request) {
+    //Handle GET request from /api/users
+    //Retireve users from the databse or any data source
+
+    const users = [
+        {id: 1 , name: "Matt"},
+        {id: 2 , name: "Michele"},
+        {id: 3 , name: "Glenn"},
+    ]
+
+    //Send the users as a response
+    return new Response(JSON.stringify(users));
+}
+
+
+
+// export async function POST(request: Request) {
+    
+// }
+
+// export async function PUT(request: Request) {
+//    ;
+// }
+
+// export async function PATCH(request: Request) {
+//     ;
+// }
+
+// export async function DELETE(request: Request) {
+    
+// }
+
+// export async function HEAD(request: Request) {
+    
+// }
+
+// export async function OPTIONS(request: Request) {
+    
+// }
